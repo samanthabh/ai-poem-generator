a445a35dea2c0345b7466a25f00f750794f542da
samanthabh <samantha.burfield-haar@octopusev.com>
2025-11-11 18:44:59 +0000

    api integration added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+    delay: 20,
+    cursor: "",
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: `Of morning light the rivers sing,
-            Soft winds awaken everything,
-            And quiet hope begins to ring.`,
-    autoStart: true,
-    delay: 30,
-    cursor: "",
-  });
+  let userInput = document.querySelector("#user-input");
+
+  let apiKey = "e4d7d87d81aef5843860374o00tff38b";
+  let prompt = `User instructions: Generate a poem about ${userInput.value}`;
+  let context =
+    "You are a poetry expert and you write beautiful poems. The poems must be no more than 6 lines long. You should have start a new line after each comma ',' using a <br /> of the poem. You must also listen carefully to the user instructions.";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log(userInput.value);
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
