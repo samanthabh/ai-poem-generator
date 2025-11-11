c5938a232a1194fdde2f85fcec402a264d89456e
samanthabh <samantha.burfield-haar@octopusev.com>
2025-11-11 19:57:45 +0000

    ux improved

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayPoem(response) {
   new Typewriter("#poem", {
     strings: response.data.answer,
     autoStart: true,
-    delay: 20,
+    delay: 10,
     cursor: "",
   });
 }
@@ -15,10 +15,14 @@ function generatePoem(event) {
   let apiKey = "e4d7d87d81aef5843860374o00tff38b";
   let prompt = `User instructions: Generate a poem about ${userInput.value}`;
   let context =
-    "You are a poetry expert and you write beautiful poems. The poems must be no more than 6 lines long. You should have start a new line after each comma ',' using a <br /> of the poem. You must also listen carefully to the user instructions.";
+    "You are a poetry expert and you write poems. The poems must be no more than 6 lines long. You should have start a new line after each comma ',' using a <br /> of the poem. You must also listen carefully to the user instructions.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log(userInput.value);
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+
+  poemElement.innerHTML = `<div class="blink">💭 Generating a poem about ${userInput.value}</div>`;
+
   axios.get(apiURL).then(displayPoem);
 }
 
